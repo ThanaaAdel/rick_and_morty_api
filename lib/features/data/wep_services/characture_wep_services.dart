f87ef17bcdd8fb95c521eed1fa3132e48d1afbbe
ThanaaAdel <swaadl971@gmail.com>
2023-09-18 10:29:21 +0300

    finsh charactures wep services

---
+++ lib/features/data/wep_services/characture_wep_services.dart
@@ -0,0 +1,15 @@
+import '../../../core/api_services.dart';
+import '../models/characters/characters.dart';
+
+class CharacturesSevices {
+  Future<List<Characters>> getAllCharactures() async {
+    List<dynamic> data =
+        await Api().get(uri: 'https://rickandmortyapi.com/api/character', token: '');
+    List<Characters> listProduct = [];
+    for (int i = 0; i < data.length; i++) {
+      listProduct.add(Characters.fromJson(data[i]));
+
+    }
+    return listProduct;
+  }
+}
